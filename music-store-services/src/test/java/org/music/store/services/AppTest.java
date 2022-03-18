b1adfd70c99f42f6d91cf1726f3780ea1bf7a90e
Tiago Resende <tiago@localhost.localdomain>
2022-03-18 10:02:33 -0300

    first commit

---
+++ music-store-services/src/test/java/org/music/store/services/AppTest.java
@@ -0,0 +1,20 @@
+package org.music.store.services;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
